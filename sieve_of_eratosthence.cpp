e90419b8fec712d9203d658ca3ad17da65815c32
Pratik Patil <pratikap41@gmail.com>
2020-08-26 12:47:05 +0530

    updated sieve_of_eratosthence

--- sieve_of_eratosthence.cpp
+++ sieve_of_eratosthence.cpp
@@ -2,15 +2,17 @@
 #include<iostream>
 #include<vector>
 #include<cstring>
+#include<vector>
 
 using namespace std;
 
-int main(){
-    int n ;
-    cout<<"Enter n :";
-    cin>>n;
-    bool prime[n];
-    memset(prime, true, n*sizeof(bool));
+// sieve algorithm
+vector<int> get_prime_numbers(int n){
+    bool* prime = new bool[n];
+    // init all index to true
+    for(int i=0; i<n; i++){
+        prime[i]= true;
+    }
     for(int i=2; i*i<=n ; i++){
         if(prime[i]){
             for(int j=i*i ; j<=n ; j+=i){
@@ -18,11 +20,23 @@ int main(){
             }
         }
     }
-    cout<<"Prime Numbers from 0 to "<<n<<" :\n";
-    for(int i=1; i<n ; i++){
+    vector<int> prime_numbers;
+    for(int i=0; i<n ; i++){
         if(prime[i]){
-            cout<<" "<<i;
+            prime_numbers.push_back(i);
         }
+    }
+    return prime_numbers;
+}
+
+int main(){
+    int n ;
+    cout<<"Enter n :";
+    cin>>n;
+    vector<int> prime_numbers = get_prime_numbers(n);
+    cout<<"Prime Numbers from 0 to "<<n<<" :\n";
+    for(int i=1; i<prime_numbers.size() ; i++){
+        cout<<" "<<prime_numbers[i];
     }
 
 }
